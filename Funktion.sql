7d735507a15cc0e593b5a8cc76b91de1633c44e4
Dominic Weiser <dominic@Dominics-MacBook-Pro.local>
2017-11-22 00:09:30 +0100

    Ausgangssituation

--- Funktion.sql
+++ Funktion.sql
@@ -1,6 +1,5 @@
 create or replace function betwnstr( a_string varchar2, a_start_pos integer, a_end_pos integer ) return varchar2 
 is
 begin
- dbms_output.put_line('Hallo DOAG');
-  return substr( a_string, a_start_pos, a_end_pos - a_start_pos + 1 );
+  return substr( a_string, a_start_pos, a_end_pos - a_start_pos + 2 );
 end;
